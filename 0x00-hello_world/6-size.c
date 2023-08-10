ad709ef990f82e1529b72a0c8b50f19c026d98fc
hindoz <hindamir.99@gmail.com>
2023-08-10 16:51:33 -0700

    task_6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %c\nSize of an int:%i\n", sizeof(char), sizeof(int));
+	printf("Size of a long int: %d\n", sizeof(long));
+	printf("Size of a long long int: %o\n", sizeof(long long));
+	printf("Size of a float: %f", sizeof(float));
+	return (0);
+}
+
