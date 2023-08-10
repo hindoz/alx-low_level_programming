c2b7bd05e868045979a4874a181de5e8a7e50b2b
hindoz <hindamir.99@gmail.com>
2023-08-10 16:58:54 -0700

    task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,12 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mystring[] = "Programming is like building a multilingual puzzle";
+	puts(mystring);
+	return (0);
+}
+
