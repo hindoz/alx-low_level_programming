25b7bbb8df4f63780b356ba21a7081cd21af7c2b
hindoz <hindamir.99@gmail.com>
2023-08-10 17:02:03 -0700

    task_4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	char mystring[] = "Programming is like building a multilingual puzzle";
+	char mystring[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(mystring);
 	return (0);
 }
