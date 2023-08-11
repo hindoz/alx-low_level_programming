986dbfb6ad06fd649952f865f41e7be928774a9a
hindoz <hindamir.99@gmail.com>
2023-08-10 20:20:49 -0700

    task_101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * This function to print exactle one phrase
+ * and send it to stderr
+ *
+ */
+void main(int)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
