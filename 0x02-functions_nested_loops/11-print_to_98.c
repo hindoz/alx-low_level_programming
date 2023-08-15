5b548ef7bd17d6f312b030ff66391fe064b970f8
hindoz <hindamir.99@gmail.com>
2023-08-15 14:54:58 -0700

    edit_task_11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,11 @@
 
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n < 99)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
+		if (n != 98)
+			printf(", ");
 		n++;
 	}
-	while (n == 98)
-		printf("%d\n", n);
-
 }
