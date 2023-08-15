84910a503e989d06c0ad962e33794711a62d5127
hindoz <hindamir.99@gmail.com>
2023-08-15 10:46:34 -0700

    task_1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i, b;
+	char str[8]= "_putchar";
+	
+		for (i = 0; i < 8; i++)
+		{putchar(str[i]);}
+	putchar('\n');
+	return 0;
+}
