28b53f957e889e090612b0733e020faba09cf9c0
hindoz <hindamir.99@gmail.com>
2023-08-15 19:34:43 -0700

    edit_task_5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,25 +12,24 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar('+');
+		_putchar(43);
 		_putchar(',');
 		_putchar(' ');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		_putchar(',');
 		_putchar(' ');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		_putchar(',');
 		_putchar(' ');
 		return (-1);
 	}
 	_putchar('\n');
-
 }
