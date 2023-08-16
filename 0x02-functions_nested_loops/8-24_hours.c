0aa140a4bd95c654844f80e6178fa9a2cf75a45d
hindoz <hindamir.99@gmail.com>
2023-08-15 20:18:20 -0700

    edit_task_8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,15 @@ void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 00; i < 24; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 00; j < 60; j++)
+		for (j = 0; j < 60; j++)
 		{
 			_putchar('0' + i);
+			_putchar((i % 10) + 48);
 			_putchar(':');
 			_putchar('0' + j);
+			_putchar((j % 10) + 48);
 			_putchar('\n');
 		}
 	}
