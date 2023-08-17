4bc4b88ee3546ee5d21734ec6b6daa0e6fe805d9
hindoz <hindamir.99@gmail.com>
2023-08-17 02:17:27 -0700

    edit_times_table_task

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * times_table - print 9 time tables starts from 0
+ *
+ * Return:  0 (Success)
+ */
+
+void times_table(void)
+{
+	int i, j, l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			l = i * j;
+			_putchar(l + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
