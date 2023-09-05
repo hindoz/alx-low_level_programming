8bc8cdfc42a76fd8be68931fb9a621650ae7c669
hindoz <hindamir.99@gmail.com>
2023-09-05 12:23:21 -0700

    malloc_task_1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,15 +9,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
-	int i = 0;
+	unsigned int i = 0;
 
 	p = malloc(sizeof(char) * size);
 
-	if (size > 0)
+	if (p == NULL)
+		return (NULL);
+
+	else if (size > 0)
 	{
-		/*p[i] = c % 16;*/
-		*p = c;
-		i++;
+		for (; i < size; i++)
+		p[i] = c;
+
 		return (p);
 	}
 	else
