837648b885f7028e6aa3af32258ed39caa2699f3
hindoz <hindamir.99@gmail.com>
2023-09-11 17:30:57 -0700

    structers_task_edit_1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,16 @@
-#ifndef _struct_dog
-#define _struct_dog
- /**
+#ifndef DOG_H
+#define DOG_H
+
+/**
   * struct dog - typedef struct print info about dog
   * @name : name of the dog
   * @age : age of it
-  * owner : owner name of the dog
+  * @owner : owner name of the dog
+  *
+  * Description: long description about this structure
   */
 
-typedef struct dog
+struct dog
 {
 char *name;
 float age;
